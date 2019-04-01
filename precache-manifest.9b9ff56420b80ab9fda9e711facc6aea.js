a0b2049d7a25f6de34d0b653871b6778343c108e
sarthakpandey <pandeysarthak1@gmail.com>
2019-04-01 19:53:23 +0530

    Updates

---
+++ precache-manifest.9b9ff56420b80ab9fda9e711facc6aea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8715b6f971a6612815b6",
+    "url": "/myself/static/css/main.e687d7d4.chunk.css"
+  },
+  {
+    "revision": "8715b6f971a6612815b6",
+    "url": "/myself/static/js/main.e58ed287.chunk.js"
+  },
+  {
+    "revision": "8e3bb94e21c8a73ad026",
+    "url": "/myself/static/js/runtime~main.5e9882c6.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/myself/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2f2f26a083f291e1a85ce2ab36e5679d",
+    "url": "/myself/index.html"
+  }
+];
